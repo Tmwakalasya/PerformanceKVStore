ee741c8846f77d62f6adad9aa67d5abe9f0a1718
Tuntufye Mwakalasya <87774338+Tmwakalasya@users.noreply.github.com>
2024-11-15 09:00:03 -0500

    Refactored code and added more tests

--- StorageEngine_test.go
+++ StorageEngine_test.go
@@ -1,50 +1,74 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestGet(t *testing.T) {
+	dataStore := &DataStore{data: make(map[string]string)}
+	dataStore.set("John", "Doe")
 
-	set("John", "Doe")
-
-	got, ok := get("John")
-
+	got, ok := dataStore.Get("John")
 	if !ok {
 		t.Errorf("get() returned false, want true")
 	}
 
-	if got.value != "Doe" {
-		t.Errorf("get() = %v, want %v", got.value, "Doe")
+	if got != "Doe" {
+		t.Errorf("get() = %v, want %v", got, "Doe")
 	}
 }
 
 func TestSet(t *testing.T) {
-	set("John", "Doe")
-	got, ok := get("John")
+	dataStore := &DataStore{data: make(map[string]string)}
+	dataStore.set("John", "Doe")
+	got, ok := dataStore.Get("John")
 	if !ok {
 		t.Errorf("get() returned false, want true")
 	}
-	if got.value != "Doe" {
-		t.Errorf("get() = %v, want %v", got.value, "Doe")
+	if got != "Doe" {
+		t.Errorf("get() = %v, want %v", got, "Doe")
 	}
-
 }
-func TestRemoveKey(t *testing.T) {
-	set("John", "Doe")
 
-	removedValue, ok := RemoveKey("John")
+func TestRemoveKey(t *testing.T) {
+	dataStore := &DataStore{data: make(map[string]string)}
+	dataStore.set("John", "Doe")
+
+	removedValue, ok := dataStore.RemoveKey("John")
 	if !ok {
 		t.Errorf("remove() returned false, want true for an existing key")
 	}
 
-	got, ok := get("John")
+	got, ok := dataStore.Get("John")
 	if ok {
 		t.Errorf("get() returned true, want false")
 	}
-	if got != nil {
+	if got != "" {
 		t.Errorf("get() returned non-nil value, want nil")
 	}
 
-	if removedValue == nil || removedValue.value != "Doe" {
+	if removedValue != "Doe" {
 		t.Errorf("remove() did not return the correct deleted value")
 	}
+}
+
+func TestConcurrency(t *testing.T) {
+	var wg sync.WaitGroup
+	dataStore := &DataStore{data: make(map[string]string)}
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			dataStore.Get("key")
+			//dataStore.set("key", "value")
+			dataStore.RemoveKey("key")
+		}()
+	}
+
+	wg.Wait()
+
+	// Validate the results of the concurrent operations
+	// ...
 }
